681fd45630a345cdb14d46422590ac23895877d1
Chopper20 <cdexsw@mailinator.com>
2017-07-20 21:21:57 +0100

    more methods added (autoboxing)

--- Java-For-Testers/src/test/java/chap004testswithotherclasses/IntegerExamplesTest.java
+++ Java-For-Testers/src/test/java/chap004testswithotherclasses/IntegerExamplesTest.java
@@ -11,6 +11,12 @@ public class IntegerExamplesTest {
         Integer four = new Integer(4); //what is a constructor method?
         assertEquals("intValue returns int 4" ,
                 4, four.intValue());
+        Integer five = 5;
+        assertEquals("intValue returns int 5" ,
+                5, five.intValue());
+        Integer six = new Integer(6);
+        assertEquals("autoboxing assignment for 6" ,  //what is autoboxing?
+                6, six.intValue());
     }
 
 
